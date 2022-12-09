bdd75f23fe460f157e8a27279af3c1a32ec16c31
Goushith <goushithcmohan@gmail.com>
2022-12-10 00:35:47 +0530

    Responsive App

---
+++ main.js
@@ -0,0 +1,43 @@
+window.addEventListener('scroll' ,()=>{
+document.querySelector('nav').classList.toggle
+('window-scroll',window.scrollY>100)
+})
+
+
+//show or hide questions
+
+const faqs =document.querySelectorAll('.faq')
+faqs.forEach(faq=>{
+    faq.addEventListener('click',()=>{
+        faq.classList.toggle('open'); 
+
+        const icon=faq.querySelector(".faq-icon i")
+        if(icon.className==='fa-solid fa-plus'){
+            icon.className='fa-solid fa-minus'
+        }else{
+            icon.className='fa-solid fa-plus'
+        }
+    })
+})
+
+
+//navbar
+
+const menu =document.querySelector(".nav_menu");
+const menuBtn =document.querySelector("#open-menu-btn ");
+const closeBtn =document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click',()=>{
+    menu.style.display="flex"
+    closeBtn.style.display="inline-block"
+    menuBtn.style.display="none"
+})
+
+const  closeNav =()=>{
+
+    menu.style.display="none"
+    closeBtn.style.display="none"
+    menuBtn.style.display="inline-block"
+}
+
+closeBtn.addEventListener('click',closeNav)
